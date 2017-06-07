5b1a970b8c8c28b6fbccf4ec9601fffe5d5b18d6
Rahul Ravikumar <rahulrav@google.com>
2017-06-06 17:06:48 -0700

    Initial commit

---
+++ flags.ts
@@ -0,0 +1,5 @@
+/** Run in profile mode ? */
+export const IS_PROFILE = false;
+
+/** should the application log ? */
+export const IS_LOG = true;
---
+++ index.ts
@@ -0,0 +1,42 @@
+import {app, BrowserWindow} from 'electron';
+import url = require('url');
+import path = require('path');
+import {log} from './logger';
+
+// retain a reference to the window, otherwise it gets gc-ed
+let w: Electron.BrowserWindow|null = null;
+
+function createWindow(): Electron.BrowserWindow {
+  log('Creating window.');
+  w = new BrowserWindow({width: 800, height: 600});
+  w.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+  // open dev tools
+  w.webContents.openDevTools();
+  w.on('close', () => {
+    // allow window to be gc-ed
+    w = null;
+  });
+  return w;
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  log('All windows closed');
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+});
+
+app.on('activate', () => {
+  log('Activating');
+  if (!!w) {
+    w = createWindow();
+  }
+});
